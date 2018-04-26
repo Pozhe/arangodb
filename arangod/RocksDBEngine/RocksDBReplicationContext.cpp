9ac863126cb2e01f1381eaea740e3b1a6da9b641
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-04-26 21:03:34 +0200

    clean up

--- arangod/RocksDBEngine/RocksDBReplicationContext.cpp
+++ arangod/RocksDBEngine/RocksDBReplicationContext.cpp
@@ -711,8 +711,8 @@ RocksDBReplicationContext::CollectionIterator::CollectionIterator(
   TRI_DEFER(ExecContext::CURRENT = old);
 
   trx.addCollectionAtRuntime(collection.name());
-  auto iterator = createGenericIterator(&trx, RocksDBColumnFamily::documents(), &logical);
-  iter = std::make_unique<RocksDBGenericIterator>(std::move(iterator));
+  auto iterator = createDocumentIterator(&trx, &logical);
+  iter = std::make_unique<RocksDBGenericIterator>(std::move(iterator)); //move to heap
 
   customTypeHandler = trx.transactionContextPtr()->orderCustomTypeHandler();
   vpackOptions.customTypeHandler = customTypeHandler.get();
